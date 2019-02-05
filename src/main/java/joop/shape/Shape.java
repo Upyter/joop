006f29117966727e0cbd458195a10814e5ede4e6
Steven <enomeral@gmx.de>
2019-02-05 15:49:01 +0100

    create shape interface

---
+++ src/main/java/joop/shape/Shape.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 Upyter
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package joop.shape;
+
+import java.awt.Graphics;
+import java.util.Optional;
+
+/**
+ * A shape that can draw itself.
+ * @since 0.6
+ */
+public interface Shape {
+    /**
+     * Draws the shape and returns the next shape to take the place of this
+     * shape. This is used to register and unregister shapes from the
+     * parent/window.
+     * @param graphics The Graphics object to draw the shape.
+     * @return The successor of this shape.
+     */
+    Optional<Shape> draw(Graphics graphics);
+}
