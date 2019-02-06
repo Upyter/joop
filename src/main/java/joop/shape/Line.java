2b277dd4ba1f341de3840eaae85300b5985b06fc
Steven <enomeral@gmx.de>
2019-02-06 17:55:09 +0100

    implement line

---
+++ src/main/java/joop/shape/Line.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2019 Upyter
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package joop.shape;
+
+import java.awt.Graphics;
+import java.util.Optional;
+import unit.color.Black;
+import unit.color.Color;
+import unit.pos.Pos;
+import unit.tuple.Tuple;
+
+/**
+ /**
+ * A line.
+ * <p>This class doesn't change its own state. Whether it is immutable or not,
+ * depends on the given constructor arguments. Additionally whether this
+ * class is thread-safe or not, depends on the given graphics instance for
+ * {@link this#draw(Graphics)}.</p>
+ * @since 0.12
+ */
+public class Line implements Shape {
+    /**
+     * The first point of the line.
+     */
+    private final Pos first;
+
+    /**
+     * The second point of the line.
+     */
+    private final Pos second;
+
+    /**
+     * The color of the line.
+     */
+    private final Color color;
+
+    /**
+     * Ctor. Creates a black line.
+     * @param first The first point of the line.
+     * @param second The second point of the line.
+     */
+    public Line(final Pos first, final Pos second) {
+        this(first, second, new Black());
+    }
+
+    /**
+     * Ctor.
+     * @param first The first point of the line.
+     * @param second The second point of the line.
+     * @param color The color of the rect.
+     */
+    public Line(final Pos first, final Pos second, final Color color) {
+        this.first = first;
+        this.second = second;
+        this.color = color;
+    }
+
+    @Override
+    public final Optional<Shape> draw(final Graphics graphics) {
+        graphics.setColor(this.color.result(java.awt.Color::new));
+        Tuple.applyOn(
+            this.first,
+            // @checkstyle ParameterName (1 line)
+            (fx, fy) -> Tuple.applyOn(
+                this.second,
+                // @checkstyle ParameterName (1 line)
+                (sx, sy) -> graphics.drawLine(fx, fy, sx, sy)
+            )
+        );
+        return Optional.of(this);
+    }
+}
---
+++ src/test/java/joop/window_and_shape/WindowAndLineTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2019 Upyter
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package joop.window_and_shape;
+
+import java.awt.Rectangle;
+import java.awt.Robot;
+import joop.shape.Line;
+import joop.window.BaseWindow;
+import net.avh4.util.imagecomparison.hamcrest.ImageComparisonMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+import unit.area.AreaOf;
+import unit.color.RGBA;
+import unit.pos.PosOf;
+
+/**
+ * Tests for the combined use of {@link BaseWindow} and {@link Line}.
+ * @since 0.12
+ */
+public class WindowAndLineTest {
+    /**
+     * {@link BaseWindow#show()} must show a black line for {@link BaseWindow}
+     * with a black {@link Line} on it.
+     * joop/src/main/java/resources/joop/window_and_shape/
+     * blackLineAndWhiteWindow.png is used as the expected image of the inner
+     * area.
+     * @throws Exception Because of Thread.sleep and Robot.
+     */
+    @Test
+    public void black() throws Exception {
+        // @checkstyle LocalFinalVariableName (6 lines)
+        final var windowWidth = 370;
+        final var windowHeight = 140;
+        final var fx = 100;
+        final var fy = 120;
+        final var sx = 200;
+        final var sy = 130;
+        new BaseWindow(
+            new AreaOf(windowWidth, windowHeight),
+            new Line(
+                new PosOf(fx, fy),
+                new PosOf(sx, sy)
+            )
+        ).show();
+        final long milliseconds = 350L;
+        Thread.sleep(milliseconds);
+        MatcherAssert.assertThat(
+            new Robot().createScreenCapture(
+                new Rectangle(0, 0, windowWidth, windowHeight)
+            ),
+            ImageComparisonMatchers.looksLike(
+                "window_and_line/blackLineOnWhiteWindow.png"
+            )
+        );
+    }
+
+    /**
+     * {@link BaseWindow#show()} must show a colored line for {@link BaseWindow}
+     * with a colored {@link Line} on it.
+     * joop/src/main/java/resources/joop/window_and_shape/
+     * coloredLineAndWhiteWindow.png is used as the expected image of the inner
+     * area.
+     * @throws Exception Because of Thread.sleep and Robot.
+     */
+    @Test
+    public void colored() throws Exception {
+        // @checkstyle LocalFinalVariableName (6 lines)
+        final var windowWidth = 450;
+        final var windowHeight = 460;
+        final var fx = 35;
+        final var fy = 100;
+        final var sx = 250;
+        final var sy = 400;
+        final var green = 255;
+        new BaseWindow(
+            new AreaOf(windowWidth, windowHeight),
+            new Line(
+                new PosOf(fx, fy),
+                new PosOf(sx, sy),
+                new RGBA(0, green, 0)
+            )
+        ).show();
+        final long milliseconds = 350L;
+        Thread.sleep(milliseconds);
+        MatcherAssert.assertThat(
+            new Robot().createScreenCapture(
+                new Rectangle(0, 0, windowWidth, windowHeight)
+            ),
+            ImageComparisonMatchers.looksLike(
+                "window_and_line/coloredLineOnWhiteWindow.png"
+            )
+        );
+    }
+}
