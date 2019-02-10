382c1e92b54b95d9f470670382bd97b599ff3cae
Steven <enomeral@gmx.de>
2019-02-10 15:28:46 +0100

    create press event

---
+++ src/main/java/joop/event/Event.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 Upyter
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package joop.event;
+
+import joop.event.mouse.Mouse;
+import unit.Overlap;
+
+/**
+ * An event based on a mouse or a keyboard for a shape.
+ * @since 0.32
+ */
+public interface Event {
+    /**
+     * Registers itself for the necessary raw event from the mouse. "Raw" means
+     * that the event is global. For example the user may click on the window
+     * and then all the events that registered themselves get the click event.
+     * Now it's up to the events to check whether it belongs to the shape.
+     * @param mouse The mouse to get the raw event from.
+     * @param overlap The overlap needed to detect whether the mouse hit the
+     *  shape.
+     */
+    void registerFor(Mouse mouse, Overlap overlap);
+}
